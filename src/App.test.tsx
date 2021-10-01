966bf4c530a9e91ba66b1fd2c03658fe8fb70d77
ben straumann <benstraum@gmail.com>
2021-10-01 17:55:45 -0500

    removed test function that was inaccurate

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,16 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
-import { fetchRoutes } from './services';
-import {RouteProps} from './types/transitApiDataTypes'
 
 test('renders primary select', () => {
   render(<App />);
   const selectElement = screen.getByText(/Select route/i);
   expect(selectElement).toBeInTheDocument();
-});
-
-test('tests api Endpoints to recieve proper datatype', async() => {
-  render(<App />);
-  const selectElement = screen.getByText(/Select directions/i);
-  expect(selectElement).not.toBeInTheDocument();
 });
