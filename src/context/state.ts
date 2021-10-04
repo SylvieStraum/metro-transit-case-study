d7a4250ef41c87ef45b90d1b2c90b6a16bd507d6
ben straumann <benstraum@gmail.com>
2021-10-04 15:12:57 -0500

    added in simpe error handling for search bar if invalid number is input

--- src/context/state.ts
+++ src/context/state.ts
@@ -5,6 +5,7 @@ interface DeparturesProviderState {
   stopDetailInfo: StopDetailProps
   routeDepartures: TimePointDepartureProps[]
   allRoutes: RouteProps[]
+  searchError?: {error:Error, stopId:number}
 }
 
 const storedStopDetails = localStorage.getItem('stopDetails')
@@ -24,6 +25,10 @@ type DeparturesProviderAction =
     type: 'SET_BASE_ROUTES',
     payload: { allRoutes: RouteProps[] }
   }
+  | {
+    type: 'SET_SEARCH_ERROR',
+    payload: { searchError?: {error:Error, stopId:number} }
+  }
 
 export const useDepartureProviderState = () => {
   //reducer establishes base items with local storage and commits it there to reduce the need to ping API for info 
@@ -35,7 +40,6 @@ export const useDepartureProviderState = () => {
           return {
             ...prevState,
             stopDetailInfo: action.payload.stopDetailInfo,
-
           }
         case 'SET_DEPARTURES':
           localStorage.setItem('departureDetails', JSON.stringify(action.payload.routeDepartures));
@@ -49,18 +53,26 @@ export const useDepartureProviderState = () => {
             ...prevState,
             routeDepartures: action.payload.allRoutes,
           }
+          case'SET_SEARCH_ERROR':
+            return{
+              ...prevState,
+              searchError:action.payload.searchError
+            }
         default:
           return {
             stopDetailInfo: {},
             routeDepartures: [],
-            allRoutes:[]
+            allRoutes:[],
+            searchError:undefined
           }
       }
     },
     {
       stopDetailInfo: storedStopDetails ? JSON.parse(storedStopDetails) : {},
       routeDepartures: storedDepartures ? JSON.parse(storedDepartures) : [],
-      allRoutes: storesAllRoutes ? JSON.parse(storesAllRoutes) : []
+      allRoutes: storesAllRoutes ? JSON.parse(storesAllRoutes) : [],
+      searchError:undefined
+      
     }
   )
 }
